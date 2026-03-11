c09371697dc9bb03ea8ce40e622ccd1645f5e5a2
ShardofGlass <107578834+ShardofGlass@users.noreply.github.com>
2026-03-11 16:49:12 -0400

    Update AutoAlignToTag.java
    I'm actively trying to make this command only read specific Apriltags, but I'm having some errors with the code. This code needs to be bugfixed, which is why it is located seperately.

--- src/main/java/frc/robot/commands/AutoAlignToTag.java
+++ src/main/java/frc/robot/commands/AutoAlignToTag.java
@@ -12,10 +12,28 @@ public class AutoAlignToTag extends Command {
 
     private final SwerveRequest.FieldCentric driveRequest = new SwerveRequest.FieldCentric();
 
+    int[] allowedTags = {10};
+    
+    private void stopAutoAlign() {
+        drivetrain.setControl(
+            driveRequest
+                .withVelocityX(0)
+                .withVelocityY(0)
+                .withRotationalRate(0)
+        );
+    }
+
+    private boolean isAllowedTag(int tid) {
+        for (int tag : allowedTags) {
+            if (tag == tid) return true;
+        }
+        return false;
+    }
+    
     //0.1 - 0.4
-    double kP_turn = -0.02;
+    double kP_turn = -0.3;
     //0.3 - 0.7
-    double kP_drive = 0.05;
+    double kP_drive = 0.5;
     //This needs to be replaced with the value given from the limelight exaclty where we want it placed when shooting.
     //this will give a direct shot of the target regardless of where we are on the field. 
     double targetTY = 0.0;
@@ -27,19 +45,23 @@ public class AutoAlignToTag extends Command {
 
     @Override
     public void execute() {
-
+        
+        int tid = (int) LimelightHelpers.getFiducialID("limelight");
+        
         if (!LimelightHelpers.getTV("limelight")) {
-            drivetrain.setControl(
-                driveRequest
-                    .withVelocityX(0)
-                    .withVelocityY(0)
-                    .withRotationalRate(0)
-            );
+            stopAutoAlign();
+            return;
+        }
+
+        if (!isAllowedTag(tid)) {
+            stopAutoAlign();
             return;
         }
 
         double tx = LimelightHelpers.getTX("limelight");
-
+        double ty = LimelightHelpers.getTY("limelight");
+        // 110cm away from the limelight to the hub (ideal shooting distance)
+        
         double turn = tx * kP_turn;
 
         if (Math.abs(tx) < 1) turn = 0;
@@ -52,6 +74,8 @@ public class AutoAlignToTag extends Command {
         );
 
         System.out.println("tx: " + tx);
+        System.out.println("ty: " + ty);
+        System.out.println("Aligning to tag: " + tid);
     }
 
     @Override
