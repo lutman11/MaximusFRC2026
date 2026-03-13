4d14bb113a33f494212b41e8169af36a12646894
lutman11 <joeman753@gmail.com>
2026-03-13 15:06:36 -0400

    Update AutoAlignToTag.java
    Reworked AutoAlign to include driving forward and backwards in the command

--- src/main/java/frc/robot/commands/AutoAlignToTag.java
+++ src/main/java/frc/robot/commands/AutoAlignToTag.java
@@ -12,13 +12,32 @@ public class AutoAlignToTag extends Command {
 
     private final SwerveRequest.FieldCentric driveRequest = new SwerveRequest.FieldCentric();
 
-    //0.1 - 0.4
+    int[] allowedTags = { 9, 10, 11, 8 };
+
+    private void stopAutoAlign() {
+        drivetrain.setControl(
+                driveRequest
+                        .withVelocityX(0)
+                        .withVelocityY(0)
+                        .withRotationalRate(0));
+    }
+
+    private boolean isAllowedTag(int tid) {
+        for (int tag : allowedTags) {
+            if (tag == tid)
+                return true;
+        }
+        return false;
+    }
+
+    // 0.1 - 0.4, make negative
     double kP_turn = -0.1;
-    //0.3 - 0.7
-    double kP_drive = 0.05;
-    //This needs to be replaced with the value given from the limelight exaclty where we want it placed when shooting.
-    //this will give a direct shot of the target regardless of where we are on the field. 
-    double targetTY = 0.0;
+    // 0.03- 0.08
+    double kP_drive = 0.04;
+    // This needs to be replaced with the value given from the limelight exaclty
+    // where we want it placed when shooting.
+    // this will give a direct shot of the target regardless of where we are on the field.
+    double targetTY = 17.0;
 
     public AutoAlignToTag(CommandSwerveDrivetrain drivetrain) {
         this.drivetrain = drivetrain;
@@ -27,50 +46,54 @@ public class AutoAlignToTag extends Command {
 
     @Override
     public void execute() {
+        System.out.println("AutoAlign started");
+
+        int tid = (int) LimelightHelpers.getFiducialID("limelight");
 
         if (!LimelightHelpers.getTV("limelight")) {
-            drivetrain.setControl(
-                driveRequest
-                    .withVelocityX(0)
-                    .withVelocityY(0)
-                    .withRotationalRate(0)
-            );
+            stopAutoAlign();
+            return;
+        }
+
+        if (!isAllowedTag(tid)) {
+            stopAutoAlign();
             return;
         }
 
         double tx = LimelightHelpers.getTX("limelight");
-        double ty = LimelightHelpers.getTY("limelight");
-
+        double ty = LimelightHelpers.getTY("limelight"); // 90 inches away from the limelight to the hub (ideal shooting
+                                                         // distance according to Marc)
+        double drive = -(ty - targetTY) * kP_drive;
         double turn = tx * kP_turn;
 
-        if (Math.abs(tx) < 1) turn = 0;
+        // Deadzone for aiming adjustment
+        if (Math.abs(tx) < 0.4)
+            turn = 0;
+
+        // Deadzone for distance adjustment
+        if (Math.abs(ty) < 0.25)
+            drive = 0;
 
         drivetrain.setControl(
-            driveRequest
-                .withVelocityX(0)
-                .withVelocityY(0)
-                .withRotationalRate(turn)
-        );
+                driveRequest
+                        .withVelocityX(drive)
+                        .withVelocityY(0)
+                        .withRotationalRate(turn));
 
         System.out.println("tx: " + tx);
         System.out.println("ty: " + ty);
+        System.out.println("Aligning to tag: " + tid);
     }
 
     @Override
     public void end(boolean interrupted) {
-        drivetrain.setControl(
-            driveRequest
-                .withVelocityX(0)
-                .withVelocityY(0)
-                .withRotationalRate(0)
-        );
+        stopAutoAlign();
+        System.out.println("AutoAlign ended");
     }
 
     @Override
     public boolean isFinished() {
-        return false;
-
-        
+        return !LimelightHelpers.getTV("limelight");
     }
-    
+
 }
