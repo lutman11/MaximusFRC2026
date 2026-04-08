4284876eff5dd1aa61e6fe24aa5bfbb02c25aeee
Anthony <28.wang.anthony@romancatholichs.com>
2026-04-08 17:11:44 -0400

    Update LinearServo.java
    Complete rework

--- main/java/frc/robot/subsystems/LinearServo.java
+++ main/java/frc/robot/subsystems/LinearServo.java
@@ -1,80 +1,44 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.Servo;
+import com.revrobotics.servohub.ServoChannel;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.math.MathUtil;
 
-public class LinearServo extends Servo {
-
-    private final double m_speed;   // mm per second
-    private final double m_length;  // mm
-
-    private double setPos = 0.0;    // target position (mm)
-    private double curPos = 0.0;    // estimated position (mm)
-
+public class LinearServo {
+    private final ServoChannel m_channel;
+    private final double m_speed; 
+    private final double m_length;
+    private double setPos = 0.0;
+    private double curPos = 0.0;
     private double lastTime;
 
-    /**
-     * Parameters for L16-R Actuonix Linear Actuators
-     *
-     * @param channel PWM channel used to control the servo
-     * @param length  max length of the servo [mm]
-     * @param speed   max speed of the servo [mm/second]
-     */
-    public LinearServo(int channel, double length, double speed) {
-        super(channel);
-
-        // Actuonix L16 pulse width bounds
-        setBoundsMicroseconds(2000, 1800, 1500, 1200, 1000);
+    public LinearServo(ServoChannel channel, double length, double speed) {
+        m_channel = channel;
         m_length = length;
         m_speed = speed;
-
+        m_channel.setPowered(true);
+        m_channel.setEnabled(true);
         lastTime = Timer.getFPGATimestamp();
     }
 
-    /**
-     * Set the target position of the actuator
-     *
-     * @param setpoint target position [mm]
-     */
     public void setPosition(double setpoint) {
         setPos = MathUtil.clamp(setpoint, 0.0, m_length);
-
-        // Servo.set() expects [0.0, 1.0]
-        double servoOutput = setPos / m_length;
-        set(servoOutput);
+        double percentExtension = setPos / m_length;
+        // Map 0-100% to 1000-2000 microseconds
+        int pulseWidth = (int) (1000 + (percentExtension * 1000));
+        m_channel.setPulseWidth(pulseWidth);
     }
 
-    /**
-     * Run this periodically to update the internal position estimate
-     */
     public void updateCurPos() {
         double currentTime = Timer.getFPGATimestamp();
         double dt = currentTime - lastTime;
         lastTime = currentTime;
-
         double maxDelta = m_speed * dt;
-
-        if (curPos < setPos - maxDelta) {
-            curPos += maxDelta;
-        } else if (curPos > setPos + maxDelta) {
-            curPos -= maxDelta;
-        } else {
-            curPos = setPos;
-        }
+        if (curPos < setPos - maxDelta) curPos += maxDelta;
+        else if (curPos > setPos + maxDelta) curPos -= maxDelta;
+        else curPos = setPos;
     }
 
-    /**
-     * @return Estimated current position [mm]
-     */
-    public double getPosition() {
-        return curPos;
-    }
-
-    /**
-     * @return true if actuator has reached target position
-     */
-    public boolean isFinished() {
-        return Math.abs(curPos - setPos) < 0.5; // 0.5 mm tolerance
-    }
+    public double getPosition() { return curPos; }
+    public boolean isFinished() { return Math.abs(curPos - setPos) < 0.5; }
 }
